304d6eb70aa80f8c272c563a123915e9b8c58b86
Rotimi Owolabi <iamrotimiowolabi@gmail.com>
2023-03-09 11:12:53 +0100

    Quick fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -5,6 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
+from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -20,6 +21,8 @@ if getenv("AUTH_TYPE") == "auth":
     auth = Auth()
 elif getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
+elif getenv("AUTH_TYPE") == "session_auth":
+    auth = SessionAuth()
 
 
 @app.errorhandler(404)
@@ -51,11 +54,16 @@ def before_request():
     handler before_request
     """
     authorized_list = ['/api/v1/status/',
-                       '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                       '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                       '/api/v1/auth_session/login/']
 
     if auth and auth.require_auth(request.path, authorized_list):
         if not auth.authorization_header(request):
             abort(401)
+        if (auth.authorization_header(request) and
+            not auth.session_cookie(request)):
+            abort(401)
+        request.current_user = auth.current_user(request)
         if not auth.current_user(request):
             abort(403)
 
